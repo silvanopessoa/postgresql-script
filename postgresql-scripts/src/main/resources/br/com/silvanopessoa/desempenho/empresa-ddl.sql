7b5f0aeaeda9d159e487bd44b46dc09670207e9e
Silvano <silvanopessoa@gmail.com>
2016-01-25 13:06:27 -0200

    Signed-off-by: Silvano <silvanopessoa@gmail.com>

---
+++ postgresql-scripts/src/main/resources/br/com/silvanopessoa/desempenho/empresa-ddl.sql
@@ -0,0 +1,12 @@
+create table empresa(
+	id       	varchar(10) CONSTRAINT pk_empresa PRIMARY KEY,
+    title       varchar(40) NOT NULL,
+    did         integer NOT NULL,
+    data_referencia   date
+)
+
+create table telefone(
+	id 
+	ddd
+
+)
