86d5173d0dd18380378084b0e38968346febeddf
Silvano <silvano.pessoa@gmail.com>
2016-02-01 08:52:15 -0200

    Signed-off-by: Silvano <silvano.pessoa@gmail.com>

--- postgresql-scripts/src/main/resources/br/com/silvanopessoa/desempenho/empresa-ddl.sql
+++ postgresql-scripts/src/main/resources/br/com/silvanopessoa/desempenho/empresa-ddl.sql
@@ -1,12 +1,31 @@
+--Empresa
+--{
+--  "id":"SBT",
+--  "dataInicioAtividade":"2015-11-30T17:06:39.769Z",
+--  "telefones":[
+--    {
+--      "ddd":"11",
+--      "numero":"26365555",
+--      "tipo": "COMERCIAL" 
+--    },
+--    {
+--      "ddd":"11",
+--      "numero":"29495577",
+--      "tipo": "RESIDENCIAL" 
+--    }
+--  ],
+--  "dataReferencia":"2015-12-30T00:00:00.000Z" 
+--}
+
+
 create table empresa(
-	id       	varchar(10) CONSTRAINT pk_empresa PRIMARY KEY,
-    title       varchar(40) NOT NULL,
-    did         integer NOT NULL,
-    data_referencia   date
+	id varchar(10) CONSTRAINT pk_empresa PRIMARY KEY,
+    data_inicio_atividade date NOT NULL,
+    data_referencia   date NOT NULL
 )
 
 create table telefone(
-	id 
-	ddd
-
+	id bigserial NOT NULL,
+	ddd character varying(3) NOT NULL,
+	numero_telefone character varying(8) NOT NULL
 )
